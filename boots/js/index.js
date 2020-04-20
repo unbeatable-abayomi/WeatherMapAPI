cf2fa7df8c09b45a94cb5c70af2b10d5fe7ceae5
abayomi <53374703+unbeatable-abayomi@users.noreply.github.com>
2020-04-20 06:43:39 +0100

    addedAm_Pm

--- boots/js/index.js
+++ boots/js/index.js
@@ -1,30 +1,23 @@
 $(document).ready(function() {
-	// $('#subMitWeather').click(function() {
-		// let city = $('#city').val();
+
 		getWeatherInfo();
-		// } else {
-			// $('#error').html('Field Cannot Be Empty');
-		// }
-	// });
+		
 });
 
-// https://api.openweathermap.org/data/2.5/forecast?q=${city},nigeria&appid=e43c60c1d74b3bccd1b3654eb8db80bd&units=metric
 
-//weather-info
 
 let date = new Date();
  let year = date.getFullYear() 
 let month = date.getMonth();
 let day = date.getDay();
 let days = date.getDate();
-let hour = date.getHours();
+let hour = date.getHours() % 12;
 let minutes = date.getMinutes();
 let seconds = date.getSeconds();
-let Dec31_1969 = new Date(-24 * 3600 * 1000);
-let checkmu = new Date().getTimezoneOffset();
 let arryOfDays= ["Sun","Mon","Tues","Wed","Thrus","Fri","Sat"];
 let arryOfMnths = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-
+let am_pm = hour >= 12 ? "pm" : "am";
+let zero = minutes <= 9? "0" : "";
 function getWeatherInfo(){
 	let lagos ='https://api.openweathermap.org/data/2.5/weather?q=lagos&appid=e43c60c1d74b3bccd1b3654eb8db80bd&lang=en&units=metric';
 			fetch(`${lagos}`)
@@ -37,7 +30,7 @@ function getWeatherInfo(){
 					weather_info += `
 					<div class=text-center>
 					<h2 class="time_days">${arryOfDays[day]} ${days} ${arryOfMnths[month]} ${year}</h2>
-					<h2 class="time_days_two">${hour}:${minutes}:${seconds}</h2>
+					<h2 class="time_days_two">${hour}:${zero}${minutes}${am_pm}</h2>
 			<h3 class="name_city"> City: ${data.name}, <span>${data.sys.country}</span> </h4>
 			<div class="section_transpratent mt-4 p-4">	
 			<h4> Weather: ${data.weather[0].main}  </h4>
@@ -57,4 +50,4 @@ function getWeatherInfo(){
 					console.log(data)
 				});
 }
-//https://api.openweathermap.org/img/w/10
+// https://unbeatable-abayomi.github.io/WeatherMapAPI/index.html
